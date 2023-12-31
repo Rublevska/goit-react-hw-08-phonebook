a074948b8bfbf4ca0713e4b045860b707b5465c5
Rublevska <valery.rublevska@gmail.com>
2023-12-31 03:57:44 +0000

    Deploying to gh-pages from @ Rublevska/goit-react-hw-08-phonebook@f8e79b30fa72509b93af6fde761c31047fb0bf9e 🚀

---
+++ static/js/75.4e7ba624.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{6075:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var c=n(184),o={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function i(){return(0,c.jsx)("div",{style:o.container,children:(0,c.jsx)("h1",{style:o.title,children:"Book of contacts of your closest people"})})}}}]);
+//# sourceMappingURL=75.4e7ba624.chunk.js.map
